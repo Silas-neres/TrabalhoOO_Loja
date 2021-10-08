dec3d73642756c405bd395a9c83c2f80ff1cced5
Silas-neres <78981008+Silas-neres@users.noreply.github.com>
2021-10-07 23:46:03 -0300

    Update Bota.java

--- trabalho/src/model/Bota.java
+++ trabalho/src/model/Bota.java
@@ -1,6 +1,20 @@
 package model;
 
+/**
+ * Dados da bota
+ * @author acer
+ * @Version 1.0(Out 2021)
+ */
+
 public class Bota extends Produto {
+    
+    /**
+	 * Guarda os dados cadastrais da bota, sendo ela:
+	 * (1)circuferencia do cano
+	 * (2)altura do salto
+	 * @param Bota
+	 * @return CircuferenciaCano, AlturaSalto
+	 */
 
     Double circunferenciaCano;
     Double alturaSalto;
